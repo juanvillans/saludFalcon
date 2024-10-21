2f5e67fa19afd69fd41b6367d3f71d7d28871186
Juan Donquis <juandonquis07@gmail.com>
2024-10-21 11:46:41 -0400

    a

---
+++ app/Http/Resources/CaseResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CaseResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+                'patient_id' => $this->patient->id,
+                'patient_ci' => $this->patient->ci,
+                'patient_name' => $this->patient->name,
+                'patient_last_name' => $this->patient->last_name,
+                'patient_phone_number' => $this->patient->phone_number,
+                'patient_sex' => $this->patient->sex,
+                'patient_date_birth' => $this->patient->date_birth,
+                'cases' => $this->cases,
+        ];
+    }
+}
