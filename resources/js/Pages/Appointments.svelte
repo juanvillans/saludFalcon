07013cef58883415c44a1da22fbee157c4d26f62
juandonquis <juandonquis07@gmail.com>
2025-04-29 15:07:54 -0400

    ni

--- resources/js/Pages/Appointments.svelte
+++ resources/js/Pages/Appointments.svelte
@@ -1,10 +1,28 @@
-<header>
-    <a href="#">
-        <img  src="/img/logoBlue.svg" alt="">
-        <span>SaludFalcón.org</span>
-    </a>
-    <h1>Reserva tu cita médica</h1>
-    <h2>En el Hospital Universitario Alfredo Van Grieken.</h2>
 
-    <img src="/img/hero.png" alt="">
-</header>
+<div class="p-5 lg:p-10 lg:pt-5">
+
+    <header class=" overflow-hidden h-fit relative w-full max-w-[1200px] mx-auto">
+        <div class="absolute top-0 left-0 z-10">
+            <a href="#">
+                <img  src="/img/logoBlue.svg" alt="">
+                <span>SaludFalcón.org</span>
+            </a>
+            <h1 class="text-white font-bold">Reserva tu 
+            
+                <div class="biggest-text">cita médica</div>
+            </h1>
+            <h2>En el Hospital Universitario Alfredo Van Grieken.</h2>
+
+        </div>
+    
+        <img src="/img/hero.png" class=" h-[400px] w-full max-w-[1200px] rounded-2xl object-cover" alt="">
+        <img src="/img/onlyDoctor.png" class="z-50 absolute top-0 left-0 h-[400px] w-full max-w-[1200px] rounded-2xl object-cover" alt="">
+
+    </header>
+</div>
+
+<style>
+    .biggest-text {
+        font-size: 12vw;
+    }
+</style>
